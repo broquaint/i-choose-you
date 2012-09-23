e1fca40ff2898534c7a2bd72118b3a3489ae2beb
Dan Brook <broq@cpan.org>
2012-09-23 22:50:09 +0100

    Basic clicky swing interface.
    Same functionality as before but now with clicks.

--- src/i_choose_you/core.clj
+++ src/i_choose_you/core.clj
@@ -1,11 +1,28 @@
 (ns i-choose-you.core
-  (use [i-choose-you.get-game :only (get-game run-game)])
+  (use [i-choose-you.get-game :only (get-game run-game)]
+       [seesaw.core]
+       [seesaw.font])
+  (require [clojure.string :as str])
   (:gen-class))
 
-(defn -main [& args]
+(def f (frame :title "I Choose You!"
+              :on-close :exit))
+
+(defn display [content]
+  (letfn [(show-content [f] (config! f :content content))]
+    (-> f show-content pack! show!)))
+
+(defn clicky-launch [e]
   (let [[game game-name] (get-game)]
     (do
-      (println "Launching -" game-name)
-      (Thread/sleep 3000)
-      (run-game game)
-      (println "Fun time is ovah!"))))
+      (invoke-now (run-game game))
+      (display (label :text (str/join " " ["Running:" game-name]))))))
+
+(def go-button
+  (button :text "Launch game ..."
+          :margin 5
+          :font   (font :size 15 :style #{:bold})
+          :listen [:action clicky-launch]))
+
+(defn -main [& args]
+  (display go-button))
