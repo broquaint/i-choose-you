9e1c45daa2d3262d2b97a32ae210d67fc82d7683
Dan Brook <broq@cpan.org>
2012-09-23 21:39:32 +0100

    Move game getting logic into separate ns.
    Possibly premature but I suspect the GUI bit is going to get fiddly.

--- src/i_choose_you/core.clj
+++ src/i_choose_you/core.clj
@@ -1,35 +1,9 @@
 (ns i-choose-you.core
-  (require [fs.core          :as fs]
-           [clj-commons-exec :as exec]))
-
-;; Hackery for fs/Java fail
-(defn path-join [& args] (clojure.string/join "/" args))
-(defn path-join-dir [d] (map #(path-join d %) (fs/list-dir d)))
-
-;; Random non-game exe patterns, + Pirate Kart, Steam & Desura
-(defn skip-exclusions [f]
-  (not-any? #(re-find % f)
-            [#"(Desura|Steam|PIRATE)" #"(?i:config|setup|unins|launch|setting|viewer|performance)"]))
-
-(defn get-game-executables [games]
-    (filter skip-exclusions
-            (flatten
-             (for [d (filter fs/directory? (path-join-dir games))]
-               (filter #(re-find #"exe$" %) (path-join-dir d))))))
-
-(defn get-games []
-  (concat
-   (path-join-dir "C:/Users/Dan/Game Shortcuts")
-   (get-game-executables "D:/Games")))
-
-(defn run-game [game-path]
-  @(exec/sh
-    ["cmd" "/C" (fs/base-name game-path)]
-    {:dir (fs/parent game-path)}))
+  (use [i-choose-you.get-game :only (get-game run-game)])
+  (:gen-class))
 
 (defn -main [& args]
-  (let [game (rand-nth (get-games))
-        game-name (clojure.string/replace (str (fs/base-name game)) #"[.]\w+$" "")]
+  (let [[game game-name] (get-game)]
     (do
       (println "Launching -" game-name)
       (Thread/sleep 3000)
---
+++ src/i_choose_you/get_game.clj
@@ -0,0 +1,33 @@
+(ns i-choose-you.get-game
+  (require [fs.core          :as fs]
+           [clj-commons-exec :as exec]
+           [clojure.string   :as str]))
+
+;; Hackery for fs/Java fail
+(defn path-join     [& args] (str/join "/" args))
+(defn path-join-dir [d]      (map #(path-join d %) (fs/list-dir d)))
+
+;; Random non-game exe patterns + Pirate Kart, Steam & Desura
+(defn skip-exclusions [f]
+  (not-any? #(re-find % f)
+            [#"(Desura|Steam|PIRATE)" #"(?i:config|setup|unins|launch|setting|viewer|performance)"]))
+
+(defn get-game-executables [games]
+    (filter skip-exclusions
+            (flatten
+             (for [d (filter fs/directory? (path-join-dir games))]
+               (filter #(re-find #"exe$" %) (path-join-dir d))))))
+
+(defn get-games []
+  (concat
+   (path-join-dir "C:/Users/Dan/Game Shortcuts")
+   (get-game-executables "D:/Games")))
+
+(defn run-game [game-path]
+  @(exec/sh
+    ["cmd" "/C" (fs/base-name game-path)]
+    {:dir (fs/parent game-path)}))
+
+(defn get-game []
+  (let [game (rand-nth (get-games))]
+    [game (clojure.string/replace (str (fs/base-name game)) #"[.]\w+$" "")]))
