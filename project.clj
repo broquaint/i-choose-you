45ef8e931166fc29622fbe4682bb6e8f772d2d0e
Dan Brook <broq@cpan.org>
2013-10-27 22:50:55 +0000

    Use latest seesaw for simplicity
    It obviates the need for the exclusions which is good. Although I'm no
    closer to finding out why specifying an icon in a frame doesn't work.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject i-choose-you "0.2.0"
+(defproject i-choose-you "0.3.0"
   :description "Play a random game"
   :url "https://github.com/broquaint/i-choose-you"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [fs "1.3.2" :exclusions [[org.clojure/clojure]]]
-                 [seesaw "1.4.2" :exclusions [[org.clojure/clojure]]]]
+                 [fs "1.3.2"]
+                 [seesaw "1.4.4"]]
   :main i-choose-you.core
   :aot [i-choose-you.core])
--- src/i_choose_you/core.clj
+++ src/i_choose_you/core.clj
@@ -1,8 +1,8 @@
 (ns i-choose-you.core
-  (use [i-choose-you.get-game :only (get-game run-game)]
-       [seesaw.core]
-       [seesaw.font])
-  (require [clojure.string :as str])
+  (:use [i-choose-you.get-game :only (get-game run-game)]
+        [seesaw.core]
+        [seesaw.font])
+  (:require [fs.core :as fs])
   (:gen-class))
 
 (def f (frame :title "I Choose You!"
@@ -18,7 +18,7 @@
         info (select f [:#info])]
     (do
       (config! go   :text "Launch another game?")
-      (config! info :text (str/join " " ["Running:" game-name]))
+      (config! info :text (str "Running: " game-name))
       (-> f pack! show!)
       ; Sometimes this will return immediately e.g a Steam game
       ; launching steam, but there's not much we can do about this.
@@ -26,20 +26,21 @@
                  #(config! info :text (str "Finished: " game-name)))))))
 
 (def go-button
-  (button :id :go
-          :text "Launch game ..."
+  (button :id     :go
+          :text   "Launch game ..."
           :margin 5
           :font   (font :size 15 :style #{:bold})
           :listen [:action clicky-launch]))
 
 (def info-label
-  (label :id :info
+  (label :id   :info
          :text "Awaiting launch ..."
          :h-text-position :center
          :v-text-position :center))
 
 (defn -main [& args]
-  (native!)
-  (display (grid-panel :items [go-button info-label]
-                       :vgap 0
-                       :rows 2)))
+  (do
+    (native!)
+    (display (grid-panel :items [go-button info-label]
+                         :vgap 0
+                         :rows 2))))
